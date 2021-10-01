b0f2cebe9cd3c5569d4a9a5ef84ccc2dcf433698
Anthony Carnero <37918263+acarn506@users.noreply.github.com>
2021-10-01 09:50:17 -0700

    read more button

--- tours/src/Tours.js
+++ tours/src/Tours.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Tour from './Tour';
 const Tours = ({ tourInfo }) => {
-  console.log('tourInfo', tourInfo)
+  
   return (
     <div>
       {tourInfo.map( tour => (
