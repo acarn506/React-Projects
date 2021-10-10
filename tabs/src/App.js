be2d82b9add6b971682306c061f4e1496988d374
Anthony Carnero <37918263+acarn506@users.noreply.github.com>
2021-10-10 12:18:23 -0700

    btn container

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -4,7 +4,8 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-    const [jobs, setJobs] = useState([])
+    const [jobInfo, setJobInfo] = useState([])
+    const [value, setValue] = useState(0)
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState()
 
@@ -12,7 +13,7 @@ function App() {
         try {
             const data = await fetch(url)
             .then(res => res.json())
-            setJobs(data)
+            setJobInfo(data)
             setLoading(false)
         } catch (err) {
             console.log(err)
@@ -21,12 +22,55 @@ function App() {
         }
     }
 
-    console.log('data', jobs)
+    console.log('data', jobInfo)
     useEffect(() => {
         fetchData()
     }, [])
 
-  return (<h2>tabs project setup</h2>)
+    const companyBtns = jobInfo.map( (job, idx) => (
+        <button key={job.id} className={`job-btn ${idx === value && 'active-btn'}`} onClick={ () => setValue(idx)}>{job.company}</button>
+    ))
+
+    if (loading) {
+        return (
+            <section className='section loading'>
+                <h1>Loading...</h1>
+            </section>
+        )
+    }
+
+    const {company, dates, duties, title} = jobInfo[value]
+
+  return (
+      <main>
+          <section className='section'>
+              <header className='title'>
+                  <h2>Experience</h2>
+                  <div className='underline'></div>
+              </header>
+              <div className='jobs-center'>
+                  <div className='btn-container'>
+                    {companyBtns}
+                  </div>
+                  <article className='job-info'>
+                      <h3>{title}</h3>
+                      <h4>{company}</h4>
+                      <p className='job-date'>{dates}</p>
+                      {
+                          duties.map( (duty, idx) => {
+                            return (
+                                <div key={idx}className='job-desc'>
+                                    <FaAngleDoubleRight className='job-icon'/>
+                                    <p>{duty}</p>
+                                </div>
+                            )
+                          })
+                      }
+                  </article>
+            </div>
+          </section>
+      </main>
+  )
 }
 
 export default App
